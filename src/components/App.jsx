11c3cc02de9ed77b002ba3ad6289cf69715dd219
Alex Bessa <alexbessa.dev@gmail.com>
2025-11-22 09:34:23 -0300

    criando estrutura inicial de componentes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,17 @@ import Button from './Button.jsx';
 const App = () => {
   return (
     <>
-
+      <Box>
+        <Display>
+          <Symbol />
+        </Display>
+        <Box>
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+        </Box>
+      </Box>
     </>
   );
 }
